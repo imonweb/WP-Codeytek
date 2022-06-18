895cbc6b9e8000d3f0e9cf16d63bd2ba3934aa9c
Imon <contact@imonweb.co.uk>
2022-06-18 01:28:53 +0100

    menu

--- inc/traits/trait-singleton.php
+++ inc/traits/trait-singleton.php
@@ -1,27 +1,48 @@
 <?php
 /**
-*
-*   
-*
- **/
+ * Singleton trait which implements Singleton pattern in any class in which this trait is used.
+ *
+ * Using the singleton pattern in WordPress is an easy way to protect against
+ * mistakes caused by creating multiple objects or multiple initialization
+ * of classes which need to be initialized only once.
+ *
+ * With complex plugins, there are many cases where multiple copies of
+ * the plugin would load, and action hooks would load (and trigger) multiple
+ * times.
+ *
+ * If you're planning on using a global variable, then you should implement
+ * this trait. Singletons are a way to safely use globals; they let you
+ * access and set the global from anywhere, without risk of collision.
+ *
+ * If any method in a class needs to be aware of "state", then you should
+ * implement this trait in that class.
+ *
+ * If any method in the class need to "talk" to another or be aware of what
+ * another method has done, then you should implement this trait in that class.
+ *
+ * If you specifically need multiple objects, then use a normal class.
+ *
+ * @package Codeytek
+ */
 
 namespace Codeytek_Theme\Inc\Traits;
 
 trait Singleton {
-  public function __construct(){
+  protected function __construct(){
 
   }
 
-  public function __clone(){
+  final protected function __clone(){
 
   }
 
   final public static function get_instance(){
+
     static $instance = [];
 
     $called_class = get_called_class();
 
-    if( !isset( $instace[ $called_class ] ) ){
+    if( ! isset( $instace[ $called_class ] ) ){
       $instance[ $called_class ] = new $called_class();
 
       do_action( sprintf( 'codeytek_theme_singleton_init%s', $called_class ) );
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -7,12 +7,9 @@
  **/
 $menu_class =  \Codeytek_Theme\Inc\Menus::get_instance();
 $header_menu_id = $menu_class->get_menu_id( 'codeytek-header-menu');
-
 $header_menus = wp_get_nav_menu_items( $header_menu_id );
  
 ?>
-
-
 
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
@@ -26,29 +23,51 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+
+      <?php 
+        if( ! empty( $header_menus ) && is_array( $header_menus ) ){
+          ?>
+      
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <?php 
+              foreach ( $header_menus as $menu_item ){
+                if( ! $menu_item->menu_item_parent ){
+                
+                  $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID  );
+                  ?>
+
+                    <!--========= Parent ==========-->
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <!--========= Parent End ==========-->
+
+                    <!--========= Child ==========-->
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                      </a>
+                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                      </ul>
+                    </li>
+                    <!--========= Child End ==========-->
+
+                  <?php 
+                }
+              }
+            ?>
+
+          </ul> <!--========= navbar-nav ==========-->
+
+          <?php 
+        }
+      ?>
+       
+
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
@@ -56,3 +75,12 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
     </div>
   </div>
 </nav>
+
+<?php
+// wp_nav_menu(
+//   [
+//     'theme_location'  =>  'codeytek-header-menu',
+//     'container_class' =>  'my_extra_menu_class'
+//   ]
+// );
+?>
