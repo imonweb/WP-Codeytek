a7edad25812c9d50f10f8d64fb6ab47d765ac238
Imon <contact@imonweb.co.uk>
2022-06-24 17:14:07 +0100

    block patterns

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -33,18 +33,22 @@ class Assets {
 
   public function register_styles(){
     /*  Register Styles */
-    wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime( CODEYTEK_DIR_PATH . '/style.css'), 'all');
+    // wp_register_style( 'style-css', get_stylesheet_uri(), [], filemtime( CODEYTEK_DIR_PATH . '/style.css'), 'all');
     wp_register_style( 'bootstrap-css', CODEYTEK_DIR_URI . '/assets/src/library/bootstrap/css/bootstrap.min.css', [], false, 'all');
+    wp_register_style( 'main-css', CODEYTEK_BUILD_CSS_URI . '/main.css', ['bootstrap-css'], filemtime( CODEYTEK_BUILD_CSS_DIR_PATH . '/main.css' ), 'all' );
 
     /*  Enqueue Styles */
-    wp_enqueue_style('style-css');
     wp_enqueue_style('bootstrap-css');
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('main-css');
+    
   }
 
   public function register_scripts(){
     /*  Register Scripts */
-    wp_register_script( 'main-js', CODEYTEK_DIR_URI . '/assets/main.js', ['jquery'], filemtime( CODEYTEK_DIR_PATH . '/assets/main.js'), true );
     // wp_register_script( 'bootstrap-js', CODEYTEK_DIR_URI . '/assets/src/library/bootstrap/js/bootstrap.min.js', [ 'jquery' ], false, true );
+    // wp_register_script( 'main-js', CODEYTEK_DIR_URI . '/assets/main.js', ['jquery'], filemtime( CODEYTEK_DIR_PATH . '/assets/main.js'), true );
+    wp_register_script( 'main-js', CODEYTEK_BUILD_JS_URI . '/main.js', ['jquery'], filemtime( CODEYTEK_BUILD_JS_DIR_PATH . '/main.js'), true );
      wp_register_script( 'bootstrap-bundle-js', CODEYTEK_DIR_URI . '/assets/src/library/bootstrap/js/bootstrap.bundle.min.js', [ 'jquery' ], false, true );
   
   
--- inc/classes/class-codeytek-theme.php
+++ inc/classes/class-codeytek-theme.php
@@ -22,6 +22,7 @@ class CODEYTEK_THEME {
     Menus::get_instance();
     Meta_Boxes::get_instance();
     Sidebars::get_instance();
+    Block_Patterns::get_instance();
 
     $this->setup_hooks();
   } // __construct
@@ -95,14 +96,66 @@ class CODEYTEK_THEME {
 			]
 		);
 
-    add_editor_style();
-    add_theme_support( 'wp-block-styles' );
-    add_theme_support( 'align-wide' );
+    // Gutenberg theme support.
 
-    global $content_width;
-    if( ! isset( $content_width )) {
-      $content_width = 1240;
-    }
+		/**
+		 * Some blocks in Gutenberg like tables, quotes, separator benefit from structural styles (margin, padding, border etc…)
+		 * They are applied visually only in the editor (back-end) but not on the front-end to avoid the risk of conflicts with the styles wanted in the theme.
+		 * If you want to display them on front to have a base to work with, in this case, you can add support for wp-block-styles, as done below.
+		 * @see Theme Styles.
+		 * @link https://make.wordpress.org/core/2018/06/05/whats-new-in-gutenberg-5th-june/, https://developer.wordpress.org/block-editor/developers/themes/theme-support/#default-block-styles
+		 */
+		add_theme_support( 'wp-block-styles' );
+
+		/**
+		 * Some blocks such as the image block have the possibility to define
+		 * a “wide” or “full” alignment by adding the corresponding classname
+		 * to the block’s wrapper ( alignwide or alignfull ). A theme can opt-in for this feature by calling
+		 * add_theme_support( 'align-wide' ), like we have done below.
+		 *
+		 * @see Wide Alignment
+		 * @link https://developer.wordpress.org/block-editor/developers/themes/theme-support/#wide-alignment
+		 */
+		add_theme_support( 'align-wide' );
+
+		/**
+		 * Loads the editor styles in the Gutenberg editor.
+		 *
+		 * Editor Styles allow you to provide the CSS used by WordPress’ Visual Editor so that it can match the frontend styling.
+		 * If we don't add this, the editor styles will only load in the classic editor ( tiny mice )
+		 *
+		 * @see https://developer.wordpress.org/block-editor/developers/themes/theme-support/#editor-styles
+		 */
+		add_theme_support( 'editor-styles' );
+		/**
+		 *
+		 * Path to our custom editor style.
+		 * It allows you to link a custom stylesheet file to the TinyMCE editor within the post edit screen.
+		 *
+		 * Since we are not passing any parameter to the function,
+		 * it will by default, link the editor-style.css file located directly under the current theme directory.
+		 * In our case since we are passing 'assets/build/css/editor.css' path it will use that.
+		 * You can change the name of the file or path and replace the path here.
+		 *
+		 * @see add_editor_style(
+		 * @link https://developer.wordpress.org/reference/functions/add_editor_style/
+		 */
+		add_editor_style( 'assets/build/css/editor.css' );
+
+		// Remove the core block patterns
+		remove_theme_support( 'core-block-patterns' );
+
+		/**
+		 * Set the maximum allowed width for any content in the theme,
+		 * like oEmbeds and images added to posts
+		 *
+		 * @see Content Width
+		 * @link https://codex.wordpress.org/Content_Width
+		 */
+		global $content_width;
+		if ( ! isset( $content_width ) ) {
+			$content_width = 1240;
+		}
 
 
   } /*  setup_theme */
