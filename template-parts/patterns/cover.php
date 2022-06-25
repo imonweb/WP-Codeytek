508654eb1e2e452f6e5df4d7fb8c9f70e8f61400
Imon <contact@imonweb.co.uk>
2022-06-25 23:10:55 +0100

    block patterns

---
+++ template-parts/patterns/cover.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Cover Block Patterns Template
+ *
+ * @package codeytek
+ */
+
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( AQUILA_BUILD_IMG_URI . '/patterns/cover.jpg' ) ?>","id":3854,"minHeight":640,"align":"full","className":"aquila-cover"} -->
+<div class="wp-block-cover alignfull has-background-dim aquila-cover" style="background-image:url(<?php echo esc_url( AQUILA_BUILD_IMG_URI . '/patterns/cover.jpg' ) ?>);min-height:640px"><div class="wp-block-cover__inner-container"><!-- wp:heading {"align":"center","level":1} -->
+		<h1 class="has-text-align-center"><strong>Never let your memories be greater than your dreams</strong></h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center","textColor":"cyan-bluish-gray"} -->
+		<p class="has-text-align-center has-cyan-bluish-gray-color has-text-color">A mind that is stretched by a new experience can never go back to its old dimensions.</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"align":"center"} -->
+		<div class="wp-block-buttons aligncenter"><!-- wp:button {"textColor":"cyan-bluish-gray","className":"is-style-outline"} -->
+			<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-cyan-bluish-gray-color has-text-color">Blogs</a></div>
+			<!-- /wp:button --></div>
+		<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->
+
+
+
+<!-- wp:cover {"url":"http://localhost/wp/codeytek/wp-content/uploads/2022/06/hotel.jpg","id":77,"dimRatio":50,"contentPosition":"center center","isDark":false,"align":"full","className":"codeytek-cover"} -->
+<div class="wp-block-cover alignfull is-light codeytek-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-77" alt="" src="http://localhost/wp/codeytek/wp-content/uploads/2022/06/hotel.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"align":"full","textColor":"white"} -->
+<h1 class="alignfull has-text-align-center has-white-color has-text-color">Never let your memories be greater than your dream</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#dedede"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#dedede">A mind that is stretched by a new experience can never go back its old dimensions.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-cyan-blue","align":"center"} -->
+<div class="wp-block-button aligncenter"><a class="wp-block-button__link has-vivid-cyan-blue-background-color has-background">Blogs</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->
